00c6c2eefecff879bc496dba69a4b165d7b6a1f8
Anupam Haldkar <ahdev2020@outlook.com>
2020-11-17 16:19:26 +0530

    React improve Component

--- src/Application.js
+++ src/Application.js
@@ -2,7 +2,11 @@ import React, {Component} from 'react';
 
 class Application extends Component {
 render(){
-    return( <h1>Hello World</h1>
+    
+    return( <div>
+           <h1>Hello World</h1>
+            <span>This is ES6</span>
+            </div>
     );
 }
 }
