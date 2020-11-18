94fd13b440ca8780c603df78134463fdd7f8e139
Anupam Haldkar <ahdev2020@outlook.com>
2020-11-18 16:42:38 +0530

    React willMount

--- src/Application.js
+++ src/Application.js
@@ -1,10 +1,19 @@
 import React, {Component} from 'react';
 
 class Application extends Component {
+    constructor(props){
+        super(props);
+    }
+    componentDidMount(props, state){
+        console.log("mount will",{props},{state});
+    }
+    componentWillMount(props, state){
+        
+    }
 render(){
-    
+    let name = "Anupam";
     return( <div>
-           <h1>Hello World</h1>
+           <h1>Hello, {name}</h1>
             <span>This is ES6</span>
             </div>
     );
